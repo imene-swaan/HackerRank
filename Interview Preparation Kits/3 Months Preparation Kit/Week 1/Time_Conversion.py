a480f13c19f7e3dab60cfa3ab4e546de8d37fad5
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-01-10 19:18:51 +0100

    Create Time_Conversion.py

---
+++ Interview Preparation Kits/3 Months Preparation Kit/Week 1/Time_Conversion.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if s[-2:] == 'PM':
+        if s[:2] == '12':
+            t = s[:-2]
+        else:
+            t = str(int(s[:2])+12) + s[2:-2]
+            
+    else:
+        if s[:2] == '12':
+            t = '00'+s[2:-2]
+        else:
+             t = s[:-2]  
+    return t
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
