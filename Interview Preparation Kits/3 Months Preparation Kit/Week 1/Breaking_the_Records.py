39fd5f96f6fc8f8d4bebaab6a0e57f2e2cf8ecbe
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-01-10 19:36:35 +0100

    Create Breaking_the_Records.py

---
+++ Interview Preparation Kits/3 Months Preparation Kit/Week 1/Breaking_the_Records.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    maximum = scores[0]
+    minimum = scores[0]
+    mx = 0
+    mn = 0
+    for i in scores:
+        if i > maximum:
+            mx +=1
+            maximum = i 
+        if i < minimum:
+            mn +=1
+            minimum = i
+    return mx , mn
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
